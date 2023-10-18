001be8fd7b2fd138bbb8100ffbfe141abe8b8a0c
maciejgz <macgzi@gmail.com>
2023-10-18 21:03:29 +0200

    spring cloud config server

--- rac-user-service/src/main/java/pl/mg/rac/user/RacUserServiceApplication.java
+++ rac-user-service/src/main/java/pl/mg/rac/user/RacUserServiceApplication.java
@@ -1,13 +1,23 @@
 package pl.mg.rac.user;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RacUserServiceApplication {
 
+    @Value("${rac.user.variable}")
+    private String value;
+
     public static void main(String[] args) {
         SpringApplication.run(RacUserServiceApplication.class, args);
+    }
+
+    @Bean
+    public void testing() {
+        System.out.println("Value: " + value);
     }
 
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,3 +2,4 @@ rootProject.name = "rac"
 include("rac-api-gateway")
 include("rac-discovery-service")
 include("rac-user-service")
+include("rac-configuration-service")
