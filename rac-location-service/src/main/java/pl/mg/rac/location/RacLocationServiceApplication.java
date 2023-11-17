437a7d69b44222e999b0dba0ade32073a3d004fa
maciejgz <macgzi@gmail.com>
2023-11-17 08:22:08 +0100

    #9 location service WIP

---
+++ rac-location-service/src/main/java/pl/mg/rac/location/RacLocationServiceApplication.java
@@ -0,0 +1,13 @@
+package pl.mg.rac.location;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RacLocationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RacLocationServiceApplication.class, args);
+    }
+
+}
