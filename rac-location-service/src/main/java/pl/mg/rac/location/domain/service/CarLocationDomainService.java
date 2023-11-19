0b6ab6ad354bb48d59d1fff37d107dd3736c6195
maciejgz <macgzi@gmail.com>
2023-11-19 14:12:11 +0100

    #9 location service WIP

--- rac-location-service/src/main/java/pl/mg/rac/location/domain/service/CarLocationDomainService.java
+++ rac-location-service/src/main/java/pl/mg/rac/location/domain/service/CarLocationDomainService.java
@@ -4,6 +4,7 @@ import pl.mg.rac.location.domain.model.CarLocation;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.security.SecureRandom;
 import java.util.List;
 
 public class CarLocationDomainService {
@@ -11,7 +12,8 @@ public class CarLocationDomainService {
     public BigDecimal calculateDistanceTraveled(List<CarLocation> positions) {
         //TODO implement calculation of distance traveled based on the positions list
         //generate random vale in BigDecimal between 1 and 100
-        return BigDecimal.valueOf(Math.random() * 100).setScale(2, RoundingMode.DOWN);
+        SecureRandom random = new SecureRandom();
+        return BigDecimal.valueOf(random.nextDouble() * 100).setScale(2, RoundingMode.DOWN);
     }
 
 }
